7fee3568b713d168b740dd25008f700c374e5e6b
Luke Davis <luke@lukealexdavis.co.uk>
2024-05-16 22:29:14 +0100

    upgraded astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import yaml from '@rollup/plugin-yaml';
 
 export default defineConfig({
   integrations: [sitemap()],
   site: "https://www.firefla.me",
-  trailingSlash: 'always'
+  trailingSlash: 'always',
+  vite: {plugins: [yaml()]}
 });
